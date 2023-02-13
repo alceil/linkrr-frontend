5ec843a558440d2eb9bec492647104cfe88633c9
alceil <ashishshajisrampickal@gmail.com>
2023-02-13 07:43:48 +0530

    Add Admin component

---
+++ src/pages/Admin/Admin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './styles.module.css'
+const Admin = () => {
+  return (
+    <div className={style.main_container}>
+        <div className={style.left_container}>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Admin
