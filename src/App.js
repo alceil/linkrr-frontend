72b6a4e07e751891be06be3d71f3d80566566923
alceil <ashishshajisrampickal@gmail.com>
2023-02-13 07:43:12 +0530

    Add routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+import { Route, Routes } from 'react-router-dom';
+import Admin from './pages/Admin/Admin';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-{/* <Login/> */}
-<Signup/>
+      <Routes>
+    <Route path="/" element={<Login/>} />
+    <Route path="/register" element={<Signup/>} />
+    <Route path="/admin" element={<Admin/>} />
+    </Routes> 
     </div>
   );
 }
