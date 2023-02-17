a51a0ca814471d3835d4ca50eafcefd253b6a452
alceil <ashishshajisrampickal@gmail.com>
2023-02-17 15:22:30 +0530

    Add functionalities for AddSocials component

--- src/components/AddSocials/AddSocials.jsx
+++ src/components/AddSocials/AddSocials.jsx
@@ -1,13 +1,84 @@
 import React from 'react'
 import style from './styles.module.css'
+import { useAdmin } from "../../contexts/adminContext";
 const AddSocials = () => {
+  const { state, dispatch } = useAdmin();
+  const {
+    twitter,
+    instagram,
+    facebook,
+    linkedin,
+    github,
+  } = state.socials;
+
   return (
     <div className={style.addsocials_container}>
     <h1 className={style.addsocials_title}>Social Icons</h1>
-    <input type="name" placeholder='Twitter handle' className={style.addsocials_inputfield}/>
-    <input type="name" placeholder='Instagram handle' className={style.addsocials_inputfield}/>
-    <input type="name" placeholder='Facebook handle' className={style.addsocials_inputfield}/>
-    <input type="name" placeholder='Linkedin handle' className={style.addsocials_inputfield}/>
+    <input 
+    type="name" 
+    placeholder='Twitter handle' 
+    value={twitter} 
+    className={style.addsocials_inputfield}
+    onChange={(e) =>
+      dispatch({
+        type: "field",
+        field: "socials",
+        value: { ...state.socials, twitter: e.target.value },
+      })
+    }
+    />
+    <input 
+    type="name" 
+    placeholder='Instagram handle' 
+    value={instagram} 
+    className={style.addsocials_inputfield}
+        onChange={(e) =>
+      dispatch({
+        type: "field",
+        field: "socials",
+        value: { ...state.socials, instagram: e.target.value },
+      })
+    }
+    />
+    <input 
+    type="name" 
+    placeholder='Facebook handle' 
+    value={facebook} 
+    className={style.addsocials_inputfield}
+        onChange={(e) =>
+      dispatch({
+        type: "field",
+        field: "socials",
+        value: { ...state.socials, facebook: e.target.value },
+      })
+    }
+    />
+    <input 
+    type="name" 
+    placeholder='Linkedin handle' 
+    value={linkedin} 
+    className={style.addsocials_inputfield}
+        onChange={(e) =>
+      dispatch({
+        type: "field",
+        field: "socials",
+        value: { ...state.socials, linkedin: e.target.value },
+      })
+    }
+    />
+    <input 
+    type="name" 
+    placeholder='GitHub handle' 
+    value={github} 
+    className={style.addsocials_inputfield}
+        onChange={(e) =>
+      dispatch({
+        type: "field",
+        field: "socials",
+        value: { ...state.socials, github: e.target.value },
+      })
+    }
+    />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AdminProvider } from "./contexts/";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AdminProvider>
+
   <Router>
    <App />
     
     </Router>  
+    </AdminProvider>
+
   </React.StrictMode>
 );
 
