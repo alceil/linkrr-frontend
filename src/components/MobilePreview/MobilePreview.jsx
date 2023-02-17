101c21b743a03605aeb1305cac5372eb69dd6cf2
alceil <ashishshajisrampickal@gmail.com>
2023-02-17 15:23:01 +0530

    Added functionalities for MobilePreview

--- src/components/MobilePreview/MobilePreview.jsx
+++ src/components/MobilePreview/MobilePreview.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import style from './styles.module.css'
+import { useAdmin } from "../../contexts/adminContext";
+import {
+  FiGithub,
+  FiLinkedin,
+  FiTwitter,
+  FiInstagram,
+  FiFacebook,
+} from "react-icons/fi";
 const MobilePreview = () => {
+const { state} = useAdmin();
+const {  profileName,about,socials} = state;
   return (
     <div className={style.mobilepreview_container}>
     <div className={style.profilepic_container}>
@@ -9,8 +19,75 @@ const MobilePreview = () => {
         </path>
         </svg>
       </div>
-      <span className={style.profile_username}>@alceil</span>
+      <span className={style.profile_username}>{profileName}</span>
     </div>
+<p className={style.about}>
+  {about}
+</p>
+<div className={style.social_container}>
+<div className={style.socials}>
+{socials.twitter && (
+  <a
+  rel="noreferrer"
+  target="_blank"
+  href={`https://twitter.com/${socials.twitter}`}>
+    <FiTwitter
+size={25}
+/>
+  </a>
+)} 
+{socials.linkedin && (
+  <a
+  rel="noreferrer"
+  target="_blank"
+  href={socials.linkedin}>
+<FiLinkedin
+size={25}
+/>
+  </a>
+
+)} 
+
+{socials.github && (
+  <a
+  rel="noreferrer"
+  target="_blank"
+  href={`https://github.com/${socials.github}`}>
+<FiGithub
+size={25}
+/>
+  </a>
+
+)} 
+
+{socials.instagram && (
+  <a
+  rel="noreferrer"
+  target="_blank"
+  href={`https://instagram.com/${socials.instagram}`}>
+<FiInstagram
+size={25}
+/>
+  </a>
+
+)} 
+{socials.facebook && (
+  <a
+  rel="noreferrer"
+  target="_blank"
+  href={`https://facebook.com/${socials.facebook}`}>
+<FiFacebook
+size={25}
+/>
+  </a>
+
+)} 
+
+
+
+</div>
+</div>
+
     <h1 className={style.website_name}>
       Linkrr
     </h1>
